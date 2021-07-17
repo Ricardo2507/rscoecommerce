e9972858d8a76f51db4d3bf02177f988c5ec17b6
Ricardo Sérgio <rscarvalho@hotmail.com>
2021-07-17 20:27:33 -0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contato/', views.contact, name='contact'),
+    path('produtos/', views.product_list, name='product_list'),
+    path('produto/', views.product, name='product'),
+    # path('<int:contato_id>', views.ver_contato, name='ver_contato'),
+]
