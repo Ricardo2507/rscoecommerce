3af81c68b41a380cf95475638bc977d4f3da2014
Ricardo Sérgio <rscarvalho@hotmail.com>
2021-07-19 11:30:55 -0300

    ajustes para deploy

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@nfaq6#qm90rpb2qu39#z8in+-wzm0m8=yr7)#b336ny==z_@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rscoecommerce.herokuapp.com']
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
