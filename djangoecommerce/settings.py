45759c5db713e01f1a84570ba529d354b2c2a331
Ricardo Sérgio <rscarvalho@hotmail.com>
2022-07-17 18:07:37 -0300

    removendo comentarios

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-SECURY_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURY_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
